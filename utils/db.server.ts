1ee51c38a04c3095112375744165f4ed59e729d4
Dhruvil <kasodariyadhruvil45@gmail.com>
2024-10-04 17:12:58 +0530

    database

---
+++ utils/db.server.ts
@@ -0,0 +1,25 @@
+// app/utils/db.server.ts
+import { PrismaClient } from "@prisma/client";
+
+let db: PrismaClient;
+
+declare global {
+  var __db: PrismaClient | undefined;
+}
+
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL_PROD,
+      },
+    },
+  });
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+  }
+  db = global.__db;
+}
+
+export { db };
